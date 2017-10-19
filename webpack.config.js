53c7517000bcc82a2670ddde65e262d30183c4c3
Patrick <patrick.munyaneza@gmail.com>
2017-10-19 19:39:30 +0200

    -added app.js app.scss - added clean and htmp webpack plugins - added sass loader - changed webpack config to handle html and sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,42 @@
-var path = require('path');
+const path = require('path');
+
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin'); 
 
 module.exports = {
+    entry: {
+        app: './src/js/app.js'
+    }, // ---- end entry
 
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js'
+    }, // ---- end output
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: [
+                    {loader: 'html-loader'}
+                ]
+            },
+
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'},
+                    {loader: 'sass-loader'}
+                ]
+            }
+        ]
+    }, // ---- end module
+
+    plugins:[
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({ template: './src/index.html'})
+    ]
 };
